73fc062b141283eb7b59f9ed8b6a2a5a32207a4f
Adriel Modollo <adrielmodollo@outlook.com>
2021-01-05 15:50:18 -0300

    Convetendo HTML em PDF

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 var pdf = require("html-pdf"); //cria variavel e instância com a biblioteca pdf
 
-pdf.create("Meu nome é Adriel e estou testando a biblioteca pdf do node",{}).toFile("./meuPdfLindao.pdf",(err, res) => {
+var conteudo = `
+
+    <h1 style='color:red'>Opa, ta ficando bonitinho! </h1>
+    <hr>
+    <p>Uau, já temos conteúdo em HTML</p>
+
+`
+
+
+
+pdf.create(conteudo,{}).toFile("./meuPdfLindao.pdf",(err, res) => {
     /* verifica existência de erro ou sucesso */  
     if(err){
         console.log("ERRO ACONTECEU OOOH NÃO...");
